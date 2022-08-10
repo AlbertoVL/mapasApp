41eb6746f803a773c273039c50c687cf81b5e5b7
AlbertoVL <jventural1500@alumno.ipn.mx>
2022-08-10 17:11:07 -0500

    5 files modified, 2 added

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: true,
+  mapboxToken: 'pk.eyJ1IjoiYWxiZXJ0b3ZsOTkiLCJhIjoiY2w2bzN4azhqMDc1eTNjcDA0YjQwMGRxZSJ9.Ekd5WRh5K7F9vLD_8lDc8w'
+}
---
+++ src/environments/environment.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: false,
+  mapboxToken: 'pk.eyJ1IjoiYWxiZXJ0b3ZsOTkiLCJhIjoiY2w2bzN4azhqMDc1eTNjcDA0YjQwMGRxZSJ9.Ekd5WRh5K7F9vLD_8lDc8w'
+}
